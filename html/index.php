e3f108f5b04a831076e2ba41a6672709722fe80f
Manuel Torres Gil <mtorres@ual.es>
2025-05-06 16:53:23 +0200

    Entorno actualizado

--- html/index.php
+++ html/index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Web PHP-MySQL con Docker</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
+
 <body>
-  <div class = "container">
+  <div class="container">
     <div class="jumbotron">
       <h1 class="display-4">Docker app</h1>
       <p class="lead">Ejemplo de aplicacion PHP y MySQL con contenedores</p>
@@ -28,28 +30,33 @@
       </thead>
       <tbody>
         <?php
-        $conexion = new mysqli("mysql", "root", "secret", "SG");
+        $host = "mysql";
+        $username = "root";
+        $password = "secret";
+        $database = "SG";
+        $mysqli = new mysqli($host, $username, $password, $database);
 
         $cadenaSQL = "select * from s_customer";
-        $resultado = $conexion->query($cadenaSQL);
+        $resultado = $mysqli->query($cadenaSQL);
 
         while ($fila = $resultado->fetch_object()) {
-         echo "<tr><td> " .$fila->name . 
-         "</td><td>" . $fila->credit_rating .
-         "</td><td>" . $fila->address .
-         "</td><td>" . $fila->city .
-         "</td><td>" . $fila->state .
-         "</td><td>" . $fila->country .
-         "</td><td>" . $fila->zip_code .
-         "</td></tr>";
-       }
+          echo "<tr><td> " . $fila->name .
+            "</td><td>" . $fila->credit_rating .
+            "</td><td>" . $fila->address .
+            "</td><td>" . $fila->city .
+            "</td><td>" . $fila->state .
+            "</td><td>" . $fila->country .
+            "</td><td>" . $fila->zip_code .
+            "</td></tr>";
+        }
 
-       ?>
-     </tbody>
-   </table>
- </div>
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        ?>
+      </tbody>
+    </table>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
+
 </html>
